93b7ee6479932fd098f82c235dbb9ca908a712eb
heping <heping@qq.com>
2023-12-08 17:46:31 +0800

    feat:新增自动化部署

--- docs/.vitepress/configs/sidebar.ts
+++ docs/.vitepress/configs/sidebar.ts
@@ -1,13 +1,15 @@
 import type { DefaultTheme } from 'vitepress'
 import TsRouter from "../router/TS"
+import ProjectRouter from "../router/project"
 export const sidebar: DefaultTheme.Config['sidebar'] = {
-  '/views/JS/':[
+  '/views/JS/': [
     {
-      text:'JS执行上下文',
-      items:[
-        { text: '从ECMAScript规范解读this', link:'/views/JS/从ECMAScript规范解读this'},
+      text: 'JS执行上下文',
+      items: [
+        { text: '从ECMAScript规范解读this', link: '/views/JS/从ECMAScript规范解读this' },
       ]
     }
   ],
-  '/views/TS/': TsRouter
+  '/views/TS/': TsRouter,
+  '/views/project/': ProjectRouter
 }
